ba68c7747910476b26a46afddc858cd944dc0c18
deepalsuranga <deepalsuranga@live.com>
2016-08-22 16:48:57 +0530

    Start project  and add data-deepalsuranga

---
+++ dfc_project2016/src/com/dfc/www/private_access/admin/cash_account/final_product_balance.java
@@ -0,0 +1,76 @@
+package com.dfc.www.private_access.admin.cash_account;
+
+
+public class final_product_balance extends javax.swing.JPanel {
+
+
+    public final_product_balance() {
+        initComponents();
+    }
+
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tbl_product = new javax.swing.JTable();
+
+        jPanel1.setLayout(null);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel1.setText("Date :");
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(30, 40, 100, 50);
+
+        jButton1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jButton1.setText("Search");
+        jButton1.setPreferredSize(new java.awt.Dimension(73, 50));
+        jPanel1.add(jButton1);
+        jButton1.setBounds(420, 40, 120, 50);
+
+        tbl_product.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Item Code", "Product name", "Quantity Added", "Balance", "Income"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tbl_product);
+
+        jPanel1.add(jScrollPane1);
+        jScrollPane1.setBounds(20, 110, 980, 260);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 1024, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 480, javax.swing.GroupLayout.PREFERRED_SIZE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tbl_product;
+    // End of variables declaration//GEN-END:variables
+}
